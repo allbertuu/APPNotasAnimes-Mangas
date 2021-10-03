7bd68d367352b5994a1cbe305e9211c89539fc4c
allbertuu <albertovinicius3@gmail.com>
2021-10-03 13:04:52 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var notaFinal = notaAnimação * 2 + notaEnredo * 2 + notaRoteiro * 3 + notaAdaptação * 3;
+// mostrar ao usuário
+for (var nota = 0.5; nota <= 10; nota = nota + 0.5) {
+	if (notaFinal < nota) {
+		var notaFinal = ;
+		"A nota final é " + notaFinal; 
+}	
+}
